527ddb7f3f0a9cc3ea970ac77091dd7f78764920
JavaNickUli <java.nickuli@mail.ru>
2023-03-08 23:25:13 +0300

    Edit Main and ProjectConfig

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -8,9 +8,23 @@ import org.springframework.context.annotation.Configuration;
 public class ProjectConfig {
 
     @Bean
-    Parrot parrot() {
+    Parrot parrot1() {
         Parrot parrot = new Parrot();
         parrot.setName("Koko");
+        return parrot;
+    }
+
+    @Bean (name = "miki")
+    Parrot parrot2() {
+        Parrot parrot = new Parrot();
+        parrot.setName("Miki");
+        return parrot;
+    }
+
+    @Bean
+    Parrot parrot3() {
+        Parrot parrot = new Parrot();
+        parrot.setName("Riki");
         return parrot;
     }
 
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,8 +8,14 @@ public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        Parrot parrot = context.getBean(Parrot.class);
-        System.out.println(parrot.getName());
+        Parrot parrot1 = context.getBean("parrot1", Parrot.class);
+        System.out.println(parrot1.getName());
+
+        Parrot parrot2 = context.getBean("miki", Parrot.class);
+        System.out.println(parrot2.getName());
+
+        Parrot parrot3 = context.getBean("parrot3", Parrot.class);
+        System.out.println(parrot3.getName());
 
         String string = context.getBean(String.class);
         System.out.println(string);
