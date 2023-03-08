1000dd252555c6397e678bb0010e3795fceb340a
JavaNickUli <java.nickuli@mail.ru>
2023-03-08 22:41:46 +0300

    Edit Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,12 +1,22 @@
 package main;
 
+import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.time.format.SignStyle;
 
 public class Main {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        Parrot parrot = new Parrot();
+        Parrot parrot = context.getBean(Parrot.class);
+        System.out.println(parrot.getName());
+
+        String string = context.getBean(String.class);
+        System.out.println(string);
+
+        Integer integer = context.getBean(Integer.class);
+        System.out.println(integer);
     }
 }
