e28968d9dc972acbf81a68698caf7de72612e737
JavaNickUli <java.nickuli@mail.ru>
2023-03-08 22:01:41 +0300

    Create Parrot

---
+++ src/main/java/uli/nick/main/Parrot.java
@@ -0,0 +1,14 @@
+package uli.nick.main;
+
+public class Parrot {
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
